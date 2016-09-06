970fc59ee0fc3801c99149616476146be9c890f5
anfavretto <anfavretto@hotmail.com>
2016-09-06 20:33:10 -0300

    06092016 - Busca de cor selecionada
    Obtenção de cor selecionada na tela.

--- Jogo/JogoCores.cpp
+++ Jogo/JogoCores.cpp
@@ -3,7 +3,9 @@
 #include <gl/glu.h>
 #include <GLFW/glfw3.h>
 #include <vector>
+#include <time.h>
 #include "Jogo.h"
+
 
 using namespace Jogo;
 using namespace std;
@@ -13,24 +15,26 @@ const int TotalRetangulosAltura = 40;
 const int LarguraJanelaPixels = 660;
 const int AlturaJanelaPixels = 500;
 const int TamanhoJanela = 40;
+const GLdouble CoordenadaInicial = 0.0;
+const GLdouble CoordenadaFinal = 40.0;
+const int CanaisRGB = 3;
 
-GLfloat xb = 0.25, yb = 0.25;
 GLfloat ultimoX = -1.0, ultimoY = -1.0;
-GLubyte r, g, b;
 vector<Retangulo> retangulos;
 vector<Retangulo>::iterator iterador;
 
 
 void init() {
-	r = g = b = 255;
-
 	//Selecionando a cor para limpar cor de fundo
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
 	//Inicializa a visualização
 	glMatrixMode(GL_PROJECTION);
-	//glLoadIdentity();
-	glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+	glLoadIdentity();
+	glOrtho(CoordenadaInicial, CoordenadaFinal, CoordenadaInicial, CoordenadaFinal, -1.0, 1.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void drawRect(float x, float y, float w, float h, float r, float g, float b) {
@@ -47,51 +51,69 @@ void render() {
 	int largura = 2;
 	int indiceRetanguloAtual = 0;
 	
-
 	float xx; // Canto 0 da tela
 	float yy = 0;
-	glBegin(GL_QUADS);
+	
 	for (int y = 0; y < TotalRetangulosAltura; y++) {
 		xx = 0;
 		for (int x = 0; x < TotalRetangulosComprimento; x++) {
 			Retangulo retanguloAtual = retangulos[indiceRetanguloAtual];
-			
+
 			if (retanguloAtual.EstaVisivel()) {
-				drawRect(xx, yy, largura, altura, retanguloAtual.ObterR(), retanguloAtual.ObterG(), retanguloAtual.ObterB());
+				glBegin(GL_QUADS);
+					drawRect(xx, yy, largura, altura, retanguloAtual.ObterR(), retanguloAtual.ObterG(), retanguloAtual.ObterB());
+				glEnd();
 			}
 			indiceRetanguloAtual++;
 			xx += largura; // aumento na largura para desenhar próximo retângulo
 		}
 		yy += altura; // aumenta altura base para desenhar próximo retângulo
 	}
-	glEnd();
-	glFlush();
+	
+}
+
+void redimensionar(GLFWwindow *window, int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(CoordenadaInicial, CoordenadaFinal, CoordenadaInicial, CoordenadaFinal, -1.0, 1.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void tratarTeclado(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)	xb += 0.01;
-	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)	xb -= 0.01;
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)	yb += 0.01;
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)	yb -= 0.01;
+	/*Sem tratamento de teclado ainda.*/
 }
 
 static void tratarPosMouse(GLFWwindow* window, double xpos, double ypos)
-{		
-	ultimoX = xpos;
-	ultimoY = ypos;
+{	
+	float xx = xpos / (float)LarguraJanelaPixels; 
+	ultimoX = xx * TamanhoJanela;
+
+	float yy = ypos / (float)AlturaJanelaPixels; 
+	ultimoY = yy * TamanhoJanela; 
 }
 
 void tratarMouse(GLFWwindow* window, int button, int action, int mods)
 {
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
 	{
-		GLvoid* pixels = new unsigned char[TamanhoJanela*TamanhoJanela*3];
-		glReadPixels(0, 0, TamanhoJanela, TamanhoJanela, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-		
-		/*string r = pixels[0];
-		string g = pixels[1];
-		string b = pixels[2];*/
+		GLubyte *pixels = new unsigned char[3];
+		//Conversão da coordenada para coordenada de tela.
+		int xTela = (ultimoX * LarguraJanelaPixels) / TamanhoJanela;
+		int yTela = (ultimoY * AlturaJanelaPixels) / TamanhoJanela;
+
+		glReadPixels(xTela, AlturaJanelaPixels - yTela, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+					
+		GLubyte *rgb = new GLubyte[CanaisRGB];
+
+		rgb[0] = pixels[0];
+		rgb[1] = pixels[1];
+		rgb[2] = pixels[2]; 
+		int a = 0;
 	}
 }
 
@@ -101,15 +123,17 @@ float ObterComponenteDeCor() {
 
 void reset() {
 	float r, g, b;
+	srand((unsigned)time(NULL));
 
 	for (int indice = 0; indice < 800; indice++) {
+		
 		r = ObterComponenteDeCor();
 		g = ObterComponenteDeCor();
 		b = ObterComponenteDeCor();
 
 		Retangulo *novoRetangulo = new Retangulo(r, g, b);
 		retangulos.push_back(*novoRetangulo);
-	}	
+	}
 }
 
 int CALLBACK WinMain(
@@ -135,21 +159,24 @@ int CALLBACK WinMain(
 	//Torna a janela o contexto ativo
 	glfwMakeContextCurrent(window);
 
+	//Define a função de callback para redimensionar
+	glfwSetWindowSizeCallback(window, redimensionar);
+
 	//Define a função de callback para tratar eventos do teclado
 	glfwSetKeyCallback(window, tratarTeclado);
 
 	//Define a função de callback para tratar eventos do mouse
 	glfwSetMouseButtonCallback(window, tratarMouse);
-	
+
 	//Define a função de callback para tratar posição do mouse
 	glfwSetCursorPosCallback(window, tratarPosMouse);
 
-	
+
 	//Inicializa a camera
 	init();
 
 	reset(); // inicializa vetor de retangulos
-		
+
 	//Faça o loop até fechar a janela
 	while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
